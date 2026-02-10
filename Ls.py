5eab3abbc62a0d2d9fb566ab99395531e6dd7b3b
Shashank Singh <135226364+Shashanksingh6529@users.noreply.github.com>
2026-02-10 16:24:14 +0530

    Add files via upload

---
+++ Ls.py
@@ -0,0 +1,17 @@
+print('Shri Harivansh Radhe Radhe')
+Marks = [99,89,100,65,92,"abc",99.9]
+print(Marks)
+print(len(Marks))
+print(Marks[4])
+
+
+Marks[0] = "Shashank"
+print(Marks[0])
+
+
+print(type(Marks))
+print(Marks[0:4])
+print(Marks[0:4])
+print(Marks[5:])
+
+print(Marks[-5:-2])
