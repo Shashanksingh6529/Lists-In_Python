9c5e4c9ac7246284302c71d42efcdfe9a3314afc
Shashank Singh <135226364+Shashanksingh6529@users.noreply.github.com>
2026-02-10 22:54:00 +0530

    Add files via upload

---
+++ LisLop.py
@@ -0,0 +1,30 @@
+print("Shri Harivansh Radhe Radhe !! ")
+
+Nums =[1,2,3,10,56,4]
+
+for No in Nums:
+    print(No)
+
+print(f"This Is The OutSide Of The Loop : {Nums}")
+
+
+"""
+
+Number = [13,12,45,10,55]
+index = 0 
+
+for val in Number:
+    index +=1
+    if val == 10:
+        print(f"The Index Is : {index}")
+        break
+"""
+Number = [13,12,45,10,55]
+index = 0 
+x = 10 
+
+for val in Number:
+    if (val == x):
+        print(f"The Index Is : {index}")
+        break
+    index += 1
---
+++ LisOp.py
@@ -0,0 +1,24 @@
+# Shri Harivansh Radhe Radhe :- 
+# Method In The List :- 
+
+lis = [1,2,3,4,5]
+
+# This Is The Append Method In The List :-  The Value Add In The Last Of The List :- 
+print(lis.append("shashank"))
+print(lis)
+
+# This Is The Insert Method In The Lists :- 
+lis.insert(0,"Singh")
+print(lis)
+
+# This Is The Sort Method In The Lists To Arrange The Value In The Sort Way:-
+a = [9,2,423,5,8,1]
+a.sort()
+a.sort(reverse = True )
+print(a)
+
+
+## This Is The Reverse Method In The List Using To Reversed The List :- 
+b = [2,5,1,0,23,3]
+b.reverse()
+print(b)
